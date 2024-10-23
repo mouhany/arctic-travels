b7a4068943e9d1f37f03e2d00efaeb30daf67777
Mouhany <mouhany@gmail.com>
2024-10-23 23:13:08 +0700

    Fixed Resorts

--- src/components/Resorts.jsx
+++ src/components/Resorts.jsx
@@ -22,27 +22,31 @@ function Resorts() {
       <div className="absolute w-full bg-gradient-to-b from-transparent via-[#d7dbdf] to-transparent h-40 z-10 -mt-20"></div>
       <section
         id="resorts"
-        className="py-12 md:py-24 min-h-screen 2xl:min-h-full bg-cover bg-center w-full flex"
+        className="py-12 md:py-24 min-h-screen 2xl:min-h-full bg-cover bg-center w-full flex items-center justify-center"
         style={{ backgroundImage: `url(${resortsImage})` }}
       >
-        <div className="w-full md:bg-neutral-50/30 lg:my-12 2xl py-24 2xl:py-36 flex">
+        <div className="w-full lg:bg-neutral-50/30 lg:my-12 flex h-full justify-center items-center lg:py-12">
           <div className="w-full max-w-screen-2xl mx-auto flex flex-col lg:flex-row lg:gap-12 items-center justify-center">
             <div className="w-full md:max-w-prose lg:w-2/5">{descResorts}</div>
 
-            <ul className="relative w-full h-full lg:w-3/5 flex flex-col gap-3">
-              {resortsData.map((resort, index) => (
-                <li
-                  key={index}
-                  className={`${
-                    index === current
-                      ? "opacity-100 absolute"
-                      : "opacity-0 absolute"
-                  } lg:top-1/2 lg:transform lg:-translate-y-1/2 transition-opacity duration-500 ease-out`}
-                >
-                  <Slideshow index={index} {...resort} />
-                </li>
-              ))}
-            </ul>
+            <div className="relative w-full lg:w-3/5 flex justify-center items-center">
+              <ul className="w-full flex flex-col gap-3 justify-center items-center relative">
+                {resortsData.map((resort, index) => (
+                  <li
+                    key={index}
+                    className={`${
+                      index === current
+                        ? "opacity-100 relative"
+                        : "opacity-0 absolute pointer-events-none"
+                    } transition-opacity duration-500 ease-out w-full flex justify-center items-center`}
+                  >
+                    <div className="w-full">
+                      <Slideshow index={index} {...resort} />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </section>
