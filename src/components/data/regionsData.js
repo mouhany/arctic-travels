b7211b4dbc8d75ebdf06080c5e9b6530d350d4d7
Mouhany <mouhany@gmail.com>
2024-10-25 00:54:56 +0700

    Update image paths to use import.meta.env.BASE_URL

--- src/components/data/regionsData.js
+++ src/components/data/regionsData.js
@@ -1,37 +1,37 @@
 export const regionsData = [
   {
     location: "Les Trois Vallées, France",
-    photo: "/assets/regions/dominik-luckmann-cPZ6W552qHA-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/dominik-luckmann-cPZ6W552qHA-unsplash.jpg`,
     url: "https://unsplash.com/photos/snow-covered-mountain-under-blue-sky-during-daytime-cPZ6W552qHA",
   },
   {
     location: "Les Portes du Soleil, Switzerland",
-    photo: "/assets/regions/reese-braam-nwTRdVTcaMY-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/reese-braam-nwTRdVTcaMY-unsplash.jpg`,
     url: "https://unsplash.com/photos/a-group-of-people-skiing-down-a-snowy-slope--XfmUvcY8XY",
   },
   {
     location: "Ski Arlberg, Austria",
-    photo: "/assets/regions/kate-banar-qE5Crd9hLgc-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/kate-banar-qE5Crd9hLgc-unsplash.jpg`,
     url: "https://unsplash.com/photos/a-man-riding-skis-down-a-snow-covered-slope-qE5Crd9hLgc",
   },
   {
     location: "Vail, USA",
-    photo: "/assets/regions/pedro-slinger-gKwpz_MR4b0-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/pedro-slinger-gKwpz_MR4b0-unsplash.jpg`,
     url: "https://unsplash.com/photos/a-person-riding-a-snowboard-down-a-snow-covered-slope-gKwpz_MR4b0",
   },
   {
     location: "Dolomiti di Brenta, Italy",
-    photo: "/assets/regions/iulia-topan-UEt6dQh-hg-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/iulia-topan-UEt6dQh-hg-unsplash.jpg`,
     url: "https://unsplash.com/photos/mountain-covered-with-snow-UEt6dQh--hg",
   },
   {
     location: "Whistler Blackcomb, Canada",
-    photo: "/assets/regions/benjamin-hayward-F-GoFKNNTW8-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/benjamin-hayward-F-GoFKNNTW8-unsplash.jpg`,
     url: "https://unsplash.com/photos/a-mountain-is-reflected-in-the-still-water-of-a-lake-F-GoFKNNTW8",
   },
   {
     location: "Niseko United, Japan",
-    photo: "/assets/regions/marek-okon-wabN84HFDjU-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/regions/marek-okon-wabN84HFDjU-unsplash.jpg`,
     url: "https://unsplash.com/photos/snow-covered-mountain-wabN84HFDjU",
   },
 ];
--- src/components/data/resortsData.js
+++ src/components/data/resortsData.js
@@ -1,17 +1,17 @@
 export const resortsData = [
   {
     location: "Valmeinier, France",
-    photo: "/assets/resorts/teo-leguay-cwL11264paM-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/resorts/teo-leguay-cwL11264paM-unsplash.jpg`,
     url: "https://unsplash.com/photos/brown-wooden-houses-surrounded-by-mountain-taken-at-nighttime-cwL11264paM",
   },
   {
     location: "Breckenridge, USA",
-    photo: "/assets/resorts/nick-rickert-LzvYLvqL-Lk-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/resorts/nick-rickert-LzvYLvqL-Lk-unsplash.jpg`,
     url: "https://unsplash.com/photos/a-ski-lodge-with-a-lot-of-snow-on-the-ground-LzvYLvqL-Lk",
   },
   {
     location: "Evolène, Switzerland",
-    photo: "/assets/resorts/xavier-von-erlach-sXwez-Xvi4Y-unsplash.jpg",
+    photo: `${import.meta.env.BASE_URL}assets/resorts/xavier-von-erlach-sXwez-Xvi4Y-unsplash.jpg`,
     url: "https://unsplash.com/photos/brown-wooden-house-on-snow-covered-ground-during-daytime-sXwez-Xvi4Y",
   },
 ];
