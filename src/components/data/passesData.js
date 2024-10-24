c7592fc90df2547fd44a760a68d372e7220828ca
Mouhany <mouhany@gmail.com>
2024-10-25 00:28:18 +0700

    Move /assets to /public and edit path

--- src/components/data/passesData.js
+++ src/components/data/passesData.js
@@ -1,62 +1,62 @@
 export const passesData = [
   {
     activity: "Skiing",
-    photo: "src/assets/passes/matthieu-petiard-Pf6e3o0GL4M-unsplash.jpg",
+    photo: "/assets/passes/matthieu-petiard-Pf6e3o0GL4M-unsplash.jpg",
     url: "https://unsplash.com/photos/man-skiing-on-land-Pf6e3o0GL4M",
   },
   {
     activity: "Snowboarding",
-    photo: "src/assets/passes/benjamin-hayward-pd6Xgu0DhxI-unsplash.jpg",
+    photo: "/assets/passes/benjamin-hayward-pd6Xgu0DhxI-unsplash.jpg",
     url: "https://unsplash.com/photos/a-man-riding-a-snowboard-down-a-snow-covered-slope-pd6Xgu0DhxI",
   },
   {
     activity: "Ski Lifts",
-    photo: "src/assets/passes/patrick-robert-doyle-svS24gSgqRs-unsplash.jpg",
+    photo: "/assets/passes/patrick-robert-doyle-svS24gSgqRs-unsplash.jpg",
     url: "https://unsplash.com/photos/red-and-black-cable-car-over-snow-covered-mountain-svS24gSgqRs",
   },
   {
     activity: "Gondola",
-    photo: "src/assets/passes/alex-wolowiecki-mkf5OjKno9g-unsplash.jpg",
+    photo: "/assets/passes/alex-wolowiecki-mkf5OjKno9g-unsplash.jpg",
     url: "https://unsplash.com/photos/a-couple-of-ski-lifts-above-a-snow-covered-mountain-mkf5OjKno9g",
   },
   {
     activity: "Snowshoeing",
-    photo: "src/assets/passes/charlie-hammond-1_Iz-bm9sQg-unsplash.jpg",
+    photo: "/assets/passes/charlie-hammond-1_Iz-bm9sQg-unsplash.jpg",
     url: "https://unsplash.com/photos/three-person-trekking-on-snow-field-mountain-at-daytime-1_Iz-bm9sQg",
   },
   {
     activity: "Snow Tubing",
-    photo: "src/assets/passes/amy-tran-GOsvNr-q2ko-unsplash.jpg",
+    photo: "/assets/passes/amy-tran-GOsvNr-q2ko-unsplash.jpg",
     url: "https://unsplash.com/photos/people-sitting-on-red-sled-on-snow-covered-ground-during-daytime-GOsvNr-q2ko",
   },
   {
     activity: "Sledding",
-    photo: "src/assets/passes/nima-sarram-WZALOIc0CxI-unsplash.jpg",
+    photo: "/assets/passes/nima-sarram-WZALOIc0CxI-unsplash.jpg",
     url: "https://unsplash.com/photos/a-group-of-people-standing-on-top-of-a-snow-covered-slope-WZALOIc0CxI",
   },
   {
     activity: "Terrain Parks",
-    photo: "src/assets/passes/6258129237_be0a1f48da_k.jpg",
+    photo: "/assets/passes/6258129237_be0a1f48da_k.jpg",
     url: "https://www.flickr.com/photos/whistlerblackcomb/6258129237/",
   },
   {
     activity: "Ice Skating",
-    photo: "src/assets/passes/anne-nygard-7cwp8CD4uqQ-unsplash.jpg",
+    photo: "/assets/passes/anne-nygard-7cwp8CD4uqQ-unsplash.jpg",
     url: "https://unsplash.com/photos/people-playing-ice-hockey-during-nighttime-7cwp8CD4uqQ",
   },
   {
     activity: "Après-ski Access",
-    photo: "src/assets/passes/39042863640_f1a679acdd_k.jpg",
+    photo: "/assets/passes/39042863640_f1a679acdd_k.jpg",
     url: "https://www.flickr.com/photos/visavis/39042863640",
   },
   {
     activity: "Winter Ziplining",
-    photo: "src/assets/passes/avel-chuklanov-48e-EBtBtmw-unsplash.jpg",
+    photo: "/assets/passes/avel-chuklanov-48e-EBtBtmw-unsplash.jpg",
     url: "https://unsplash.com/photos/trees-coated-with-snow-48e-EBtBtmw",
   },
   {
     activity: "Snowmobile Tours",
-    photo: "src/assets/passes/nate-johnston-TBRVu6ScH6c-unsplash.jpg",
+    photo: "/assets/passes/nate-johnston-TBRVu6ScH6c-unsplash.jpg",
     url: "https://unsplash.com/photos/snowmobile-speeding-on-snow-near-tree-lines-TBRVu6ScH6c",
   },
 ];
--- src/components/data/regionsData.js
+++ src/components/data/regionsData.js
@@ -1,37 +1,37 @@
 export const regionsData = [
   {
     location: "Les Trois Vallées, France",
-    photo: "src/assets/regions/dominik-luckmann-cPZ6W552qHA-unsplash.jpg",
+    photo: "/assets/regions/dominik-luckmann-cPZ6W552qHA-unsplash.jpg",
     url: "https://unsplash.com/photos/snow-covered-mountain-under-blue-sky-during-daytime-cPZ6W552qHA",
   },
   {
     location: "Les Portes du Soleil, Switzerland",
-    photo: "src/assets/regions/reese-braam-nwTRdVTcaMY-unsplash.jpg",
+    photo: "/assets/regions/reese-braam-nwTRdVTcaMY-unsplash.jpg",
     url: "https://unsplash.com/photos/a-group-of-people-skiing-down-a-snowy-slope--XfmUvcY8XY",
   },
   {
     location: "Ski Arlberg, Austria",
-    photo: "src/assets/regions/kate-banar-qE5Crd9hLgc-unsplash.jpg",
+    photo: "/assets/regions/kate-banar-qE5Crd9hLgc-unsplash.jpg",
     url: "https://unsplash.com/photos/a-man-riding-skis-down-a-snow-covered-slope-qE5Crd9hLgc",
   },
   {
     location: "Vail, USA",
-    photo: "src/assets/regions/pedro-slinger-gKwpz_MR4b0-unsplash.jpg",
+    photo: "/assets/regions/pedro-slinger-gKwpz_MR4b0-unsplash.jpg",
     url: "https://unsplash.com/photos/a-person-riding-a-snowboard-down-a-snow-covered-slope-gKwpz_MR4b0",
   },
   {
     location: "Dolomiti di Brenta, Italy",
-    photo: "src/assets/regions/iulia-topan-UEt6dQh-hg-unsplash.jpg",
+    photo: "/assets/regions/iulia-topan-UEt6dQh-hg-unsplash.jpg",
     url: "https://unsplash.com/photos/mountain-covered-with-snow-UEt6dQh--hg",
   },
   {
     location: "Whistler Blackcomb, Canada",
-    photo: "src/assets/regions/benjamin-hayward-F-GoFKNNTW8-unsplash.jpg",
+    photo: "/assets/regions/benjamin-hayward-F-GoFKNNTW8-unsplash.jpg",
     url: "https://unsplash.com/photos/a-mountain-is-reflected-in-the-still-water-of-a-lake-F-GoFKNNTW8",
   },
   {
     location: "Niseko United, Japan",
-    photo: "src/assets/regions/marek-okon-wabN84HFDjU-unsplash.jpg",
+    photo: "/assets/regions/marek-okon-wabN84HFDjU-unsplash.jpg",
     url: "https://unsplash.com/photos/snow-covered-mountain-wabN84HFDjU",
   },
 ];
